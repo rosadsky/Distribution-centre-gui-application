d369e17daaff0470c859837dc857f4616b9d010e
rosadsky <rosadsky@hotmail.com>
2021-03-10 19:12:20 +0100

    Test commit

--- src/distribution/centre/Main.java
+++ src/distribution/centre/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //moj kodddsadasdassag
+
+        System.out.println("Tibor Ten");
+
+
+
+
+        System.out.println("Zas");
     }
 }
